8468e234a36141dc5708fdaf95fb2f5abc315bee
wbrumley <wbrumley1@gmail.com>
2025-01-16 17:07:55 -0500

    manually start agent

--- .platform/hooks/prebuild/install_zabbix_agent.sh
+++ .platform/hooks/prebuild/install_zabbix_agent.sh
@@ -10,7 +10,7 @@ sudo dnf upgrade -y
 
 # Install the Zabbix agent
 sudo rpm -Uvh https://repo.zabbix.com/zabbix/7.0/amazonlinux/2023/x86_64/zabbix-release-latest-7.0.amzn2023.noarch.rpm
-sudo dnf install -y zabbix-agent
+sudo dnf install -y zabbix-agent --exclude=zabbix-tools
 
 # Configure the agent
 sudo sed -i "s/^Server=127.0.0.1/Server=$ZABBIX_SERVER_IP/" $ZABBIX_AGENT_CONF
@@ -18,5 +18,4 @@ sudo sed -i "s/^ServerActive=127.0.0.1/ServerActive=$ZABBIX_SERVER_IP/" $ZABBIX_
 sed -i "s/^Hostname=Zabbix server/Hostname=hostname/" $ZABBIX_AGENT_CONF
 
 # Manually start the Zabbix agent
-sudo systemctl enable zabbix-agent
-sudo systemctl start zabbix-agent
+sudo /usr/sbin/zabbix_agentd
