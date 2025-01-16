a858dedddd4318c34428f2758f6df4e5a1861182
wbrumley <wbrumley1@gmail.com>
2025-01-16 16:40:48 -0500

    added platform/hooks folders

--- .platform/hooks/prebuild/install_zabbix_agent.sh
+++ .platform/hooks/prebuild/install_zabbix_agent.sh
@@ -15,6 +15,7 @@ sudo dnf install -y zabbix-agent
 # Configure the agent
 sudo sed -i "s/^Server=127.0.0.1/Server=$ZABBIX_SERVER_IP/" $ZABBIX_AGENT_CONF
 sudo sed -i "s/^ServerActive=127.0.0.1/ServerActive=$ZABBIX_SERVER_IP/" $ZABBIX_AGENT_CONF
+sed -i "s/^Hostname=Zabbix server/Hostname=hostname/" $ZABBIX_AGENT_CONF
 
 # Manually start the Zabbix agent
 zabbix_agentd -c /etc/zabbix/zabbix_agentd.conf
