cd9b87d212b4d00fe868057df82890583475ab1f
Will Brumley <wbrumley1@gmail.com>
2025-01-16 13:29:32 -0500

    Update install_zabbix_agent.sh
    switched from dnf to yum

--- install_zabbix_agent.sh
+++ install_zabbix_agent.sh
@@ -6,11 +6,11 @@ ZABBIX_SERVER_IP="x.x.x.x"
 ZABBIX_AGENT_CONF="/etc/zabbix/zabbix_agentd.conf"
 
 # Update packages
-sudo dnf update -y
+sudo yum update -y
 
 # Install the Zabbix agent
-sudo rpm -Uvh https://repo.zabbix.com/zabbix/7.0/amazonlinux/2023/x86_64/zabbix-release-latest-7.0.amzn2023.noarch.rpm
-sudo dnf install -y zabbix-agent
+sudo yum -Uvh https://repo.zabbix.com/zabbix/7.0/amazonlinux/2023/x86_64/zabbix-release-latest-7.0.amzn2023.noarch.rpm
+sudo yum install -y zabbix-agent
 
 # Configure the agent
 sudo sed -i "s/^Server=127.0.0.1/Server=$ZABBIX_SERVER_IP/" $ZABBIX_AGENT_CONF
